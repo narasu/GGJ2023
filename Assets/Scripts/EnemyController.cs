02293831d6a07c258d82aaa160068e27798d7042
Adam Bausch <Fireworksanimations21@gmail.com>
2023-02-04 12:31:49 +0100

    player controller en enemy controller

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public NavMeshAgent Enemy;
+    public Transform Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy.SetDestination(Player.position);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent Agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
